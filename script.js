970d09fe008d5fc5061631cbad322a3bdfa1900c
sobugkaiserkafi <sobugkaiserkafi@gmail.com>
2025-04-30 11:13:26 +0600

    Initial website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function searchProduct() {
+  const term = document.getElementById("searchBox").value;
+  alert("সার্চ: " + term);
+}
+function openChat() {
+  const chatBox = document.getElementById("live-chat-box");
+  chatBox.style.display = "block";
+}
